e4844074c98a034ff2cf6d9be2d7ce54e6425eb6
allainwonderland <alla@inwonderland.com>
2021-08-06 20:18:27 -0700

    New changes

--- Chapter02/config.py
+++ Chapter02/config.py
@@ -11,4 +11,5 @@ class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     PYMYSQL_DATABASE_URI ='mysql+pymysql://user:password@ip:port/db_name'
     CX_ORACLE_DATABASE_URI = 'oracle+cx_oracle://user:password@ip:port/db_name'
-    
+
+SQLALCHEMY_ECHO = True.
