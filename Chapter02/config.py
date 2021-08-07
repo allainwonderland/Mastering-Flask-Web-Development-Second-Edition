0b9b58b37aad53fdfebfe3c4621dd99281aacdc2
allainwonderland <alla@inwonderland.com>
2021-08-06 17:05:36 -0700

    Changes for today

--- Chapter02/config.py
+++ Chapter02/config.py
@@ -9,3 +9,6 @@ class ProdConfig(Config):
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    PYMYSQL_DATABASE_URI ='mysql+pymysql://user:password@ip:port/db_name'
+    CX_ORACLE_DATABASE_URI = 'oracle+cx_oracle://user:password@ip:port/db_name'
+    
